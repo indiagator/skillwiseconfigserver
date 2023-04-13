4b69fc8dbc26a35a6eafe245f8e497c3a0379337
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-04-13 13:30:22 +0530

    first commit

---
+++ src/test/java/com/skillwise/skillwiseconfigserver/SkillwiseconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillwise.skillwiseconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillwiseconfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
